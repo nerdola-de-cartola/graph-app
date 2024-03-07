c78017cfc913d32b55126f41671cca44223797e7
Matheus Lucas <nerdola.cartola@gmail.com>
2024-03-07 00:34:21 -0300

    Adding class app

--- src/index.ts
+++ src/index.ts
@@ -1,68 +1,4 @@
-import type Graph from './graph/graph';
-import Line from './view/line';
-import Circle from './view/circle';
-import { linePointNearestPoint, distance, Point, drawPerimeter } from './view/geometry';
-import VisualGraph from './view/visual-graph';
-import type Vertex from './graph/vertex';
-import { bfs, dfs, search } from './graph/graph-algorithms';
-
-interface Animation {
-  obj: any
-  animation: any
-  params: any
-  duration: number
-}
-
-class Animations {
-  list: Animation[]
-  startTime: number
-  currentDuration: number
-
-  constructor() {
-    this.list = [];
-    this.startTime = performance.now();
-    this.currentDuration = 0;
-  }
-
-  addAnimation(animation: Animation) {
-    this.list.push(animation)
-
-    if(this.list.length ===  1) {
-      requestAnimationFrame(() => this.animate())
-    }
-  }
-
-  animate() {
-    const now = performance.now();
-    const elapsedTime = now - this.startTime;
-
-    if (elapsedTime >= this.currentDuration) {
-      const item = this.list.shift();
-
-      if (!item) return;
-
-      const { obj, animation, params, duration } = item;
-      obj.animation = animation;
-      obj.animation(...params);
-      this.startTime = performance.now();
-      this.currentDuration = duration;
-    }
-
-    requestAnimationFrame(() => this.animate());
-  }
-}
-
-enum Modes {
-  moveVertex,
-  newVertex,
-  newEdge,
-  deleteVertex,
-  deleteEdge,
-  connectedComponents,
-  dfs,
-  bfs,
-  bipartiteGraph
-}
+import GraphApp, { Modes } from "./view/app";
 
 const nameModes = [
   "Move vertex",
@@ -76,70 +12,27 @@ const nameModes = [
   "Bipartite graph?"
 ]
 
-function randomHexadecimalColor() {
-  const color = Math.floor(Math.random() * 16777215).toString(16);
-  return `#${color}`
-}
-
-function drawConnectedComponents(ctx: CanvasRenderingContext2D, components: Graph[]) {
-  components.forEach(component =>
-    drawPerimeter(ctx, component.vertices as Circle[], randomHexadecimalColor())
-  );
-}
-
 function main() {
   const canvas: HTMLCanvasElement | null = document.querySelector('.mainCanvas');
+  const textEl = document.querySelector(`.textContainer`);
 
-  if (!canvas) return;
+  if (!canvas || !textEl) throw new Error('No html elements for app');
 
-  const proportion = 0.95;
-
-  canvas.width = proportion * window.innerWidth;
-  canvas.height = proportion * window.innerHeight;
-  
-  const context = canvas && canvas.getContext("2d");
-
-  if(!context) return;
-
-  const g = new VisualGraph(context);
-  const animationList = new Animations();
-
-  
-  if (!context) return;
-  
-  addEventListener('resize', () => {
-    canvas.width = proportion * window.innerWidth;
-    canvas.height = proportion * window.innerHeight;
-    reDraw();
-  })
-
-  const setMode = (newMode: number) => {
-    if(newMode === mode) return;
-
-    const oldButton: HTMLElement | null = document.querySelector(`.modeButton#b${mode}`)
-    const newButton: HTMLElement | null = document.querySelector(`.modeButton#b${newMode}`)
-
-    if(oldButton && newButton) {
-      newButton.style.backgroundColor = 'green';
-      oldButton.style.backgroundColor = '';
-    }
-
-    mode = newMode;
-
-    onModeChange();
-  }
+  const app = new GraphApp(canvas, textEl as HTMLElement);
 
   interface AddButtonsType {
     name: string,
     id: number,
-    onClick: (e: MouseEvent) => unknown
+    onClick: (e: MouseEvent) => unknown,
+    backgroundColor: string
   }
-  
+
   const addButtons = (functionality: AddButtonsType) => {
     const button = document.createElement("button");
     button.textContent = functionality.name;
     button.className = "modeButton";
     button.id = 'b' + functionality.id;
+    button.style.backgroundColor = functionality.backgroundColor;
     button.onclick = functionality.onClick;
     const wrapper = document.querySelector(".header");
     wrapper && wrapper.appendChild(button);
@@ -153,229 +46,22 @@ function main() {
   modesValues.forEach((mode, index) => addButtons({
     name: nameModes[index],
     id: index,
-    onClick: () => setMode(index)
+    onClick: () => app.setMode(index),
+    backgroundColor: index === 0 ? 'green' : ''
   }))
 
-  const mouse: Point = { x: 0, y: 0 };
-  let isDragging = false;
-  let touchVertex: Circle | undefined = undefined;
-  let touchEdge: Line | undefined = undefined;
-  let selectedVertex1: Circle | undefined = undefined;
-  let autoName = 0;
-  let mode = Modes.moveVertex;
-  let text = "";
-  let cursor = "";
+  app.canvas.addEventListener("onModeChange", (e) => {
+    //@ts-ignore
+    const oldButton: HTMLElement | null = document.querySelector(`.modeButton#b${e.detail.oldMode}`)
+    //@ts-ignore
+    const newButton: HTMLElement | null = document.querySelector(`.modeButton#b${e.detail.newMode}`)
 
-  const reDraw = () => {
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    g.draw();
-  }
-
-  const setText = (str: string) => {
-    if(str === text) return; 
-
-    const textEl = document.querySelector(`.textContainer`)
-
-    if(textEl) textEl.textContent = str;
-  }
-
-  const onModeChange = () => {
-    if (selectedVertex1) {
-      selectedVertex1.changeColor(context);
-      selectedVertex1 = undefined;
+    if (oldButton && newButton) {
+      newButton.style.backgroundColor = 'green';
+      oldButton.style.backgroundColor = '';
     }
 
-    reDraw();
-
-    switch (mode) {
-      case Modes.moveVertex:
-        setText("CLICK AND DRAG THE DESIRED VERTEX");
-        break;
-
-      case Modes.newVertex:
-        setText("CLICK TO ADD NEW VERTEX");
-        break;
-
-      case Modes.newEdge:
-        setText("SELECT FIRST VERTEX");
-        break;
-
-      case Modes.deleteVertex:
-        setText("SELECT VERTEX TO REMOVE");
-        break;
-
-      case Modes.deleteEdge:
-        setText("SELECT EDGE TO REMOVE");
-        break;
-
-      case Modes.connectedComponents:
-        setText("EACH COLORED SQUARE REPRESENTS A CONNECTED COMPONENT");
-        drawConnectedComponents(context, g.connectedComponents());
-        break;
-
-      case Modes.dfs:
-        setText("SELECT START VERTEX");
-        break;
-
-      case Modes.bfs:
-        setText("SELECT START VERTEX");
-        break;
-
-      case Modes.bipartiteGraph:
-        setText(g.bipartiteGraph() ? "TRUE" : "FALSE");
-        break;
-    }
-
-    const drawPath = (ctx: CanvasRenderingContext2D, f: any, startVertex: Vertex) => {
-      reDraw();
-
-      const r = search({
-        graph: g,
-        startVertex,
-        searchAlgorithm: f
-      });
-
-      const path = r as Circle[];
-      path.forEach((vertex: Circle, index: number) => {
-        let animation = vertex.outline;
-
-        if (index === path.length - 1) { //last animation
-          animation = (ctx) => {
-            setText("SELECT START VERTEX");
-            vertex.outline(ctx);
-          }
-        }
-
-        animationList.addAnimation({
-          obj: vertex,
-          animation,
-          params: [ctx],
-          duration: 1000
-        })
-      })
-    }
-
-    const setCursor = (c: string) => {
-      if(cursor === c) return;
-      canvas.style.cursor = c;
-    }
-
-    const mouseMove = (e: MouseEvent) => {
-      // @ts-ignore 
-      const canvasObj = e.target.getBoundingClientRect();
-      mouse.x = e.clientX - canvasObj.left;
-      mouse.y = e.clientY - canvasObj.top;
-
-
-      touchVertex = g.vertices.find((circle) => {
-        return distance(mouse, circle) <= circle.radius;
-      })
-
-      touchEdge = g.edges.find(line => {
-        const linePointNearestMouse = linePointNearestPoint(line, mouse);
-        if (!linePointNearestMouse) return false;
-        return distance(mouse, linePointNearestMouse) <= line.thickness;
-      });
-
-      if ((touchVertex || touchEdge)) {
-        setCursor("pointer");
-      } else {
-        setCursor("default");
-      }
-
-      if (isDragging) {
-        if (!selectedVertex1) return;
-
-        selectedVertex1.updatePosition(mouse.x, mouse.y);
-        reDraw();
-      }
-    }
-
-    const mouseClick = (e: MouseEvent) => {
-      // console.log(g)
-
-      switch (mode) {
-        case Modes.newVertex:
-          g.addVertex(new Circle(autoName.toString(), mouse.x, mouse.y))
-          autoName++;
-          break;
-
-        case Modes.newEdge:
-          if (!touchVertex) return;
-
-          if (!selectedVertex1) {
-            selectedVertex1 = touchVertex;
-            selectedVertex1.changeColor(context, 'red');
-            setText("SELECT SECOND VERTEX");
-          } else {
-            g.addEdge(new Line(selectedVertex1, touchVertex));
-            selectedVertex1.changeColor(context);
-            selectedVertex1 = undefined;
-            setText("SELECT FIRST VERTEX");
-          }
-
-          break;
-
-        case Modes.deleteVertex:
-          if (!touchVertex) return;
-
-          selectedVertex1 = touchVertex;
-          g.deleteVertex(selectedVertex1);
-          selectedVertex1 = undefined;
-          reDraw();
-          break;
-
-        case Modes.deleteEdge:
-          if (!touchEdge) return;
-
-          g.deleteEdge(touchEdge);
-          reDraw();
-          break;
-
-        case Modes.dfs:
-          if (!touchVertex) return;
-
-          setText("RUNNING DFS");
-          drawPath(context, dfs, touchVertex);
-          break;
-
-        case Modes.bfs:
-          if (!touchVertex) return;
-
-          setText("RUNNING BFS");
-          drawPath(context, bfs, touchVertex);
-          break;
-      }
-    }
-
-    const mouseDown = (e: MouseEvent) => {
-      switch (mode) {
-        case Modes.moveVertex:
-          if (!touchVertex) return;
-
-          selectedVertex1 = touchVertex;
-          selectedVertex1.changeColor(context, 'red')
-          isDragging = true;
-          break;
-      }
-    }
-
-    const mouseUp = (e: MouseEvent) => {
-      if (isDragging) {
-        isDragging = false;
-
-        if (!selectedVertex1) return;
-
-        selectedVertex1.changeColor(context)
-        selectedVertex1 = undefined;
-      }
-    }
-
-    canvas.onclick = mouseClick;
-    canvas.onmousedown = mouseDown;
-    canvas.onmouseup = mouseUp;
-    canvas.onmousemove = mouseMove;
-  }
-
+  })
 }
+
 main();
---
+++ src/view/app.ts
@@ -0,0 +1,347 @@
+import type Vertex from "src/graph/vertex";
+import Circle from "./circle";
+import { distance, drawPerimeter, linePointNearestPoint } from "./geometry";
+import type { Point } from "./geometry";
+import Line from "./line";
+import VisualGraph from "./visual-graph";
+import { bfs, dfs, search } from "../graph/graph-algorithms";
+
+function randomHexadecimalColor() {
+    const color = Math.floor(Math.random() * 16777215).toString(16);
+    return `#${color}`
+}
+
+export enum Modes {
+    moveVertex,
+    newVertex,
+    newEdge,
+    deleteVertex,
+    deleteEdge,
+    connectedComponents,
+    dfs,
+    bfs,
+    bipartiteGraph
+}
+
+export default class GraphApp {
+    canvas: HTMLCanvasElement
+    proportion: number
+    ctx: CanvasRenderingContext2D
+    graph: VisualGraph
+    animationList: Animations
+    mouse: Point
+    isDragging: boolean
+    touchVertex: Circle | undefined
+    touchEdge: Line | undefined
+    selectedVertex: Circle | undefined
+    autoName: number;
+    mode = Modes.moveVertex;
+    textEl: HTMLElement
+    text: string;
+    cursor: string;
+
+    constructor(canvas: HTMLCanvasElement, textEl: HTMLElement, proportion: number | undefined = 0.95) {
+        this.canvas = canvas;
+        const tmp = canvas.getContext("2d");
+        if (!tmp) throw new Error('No context 2D in canvas');
+        this.ctx = tmp;
+        this.proportion = proportion;
+        this.graph = new VisualGraph(this.ctx);
+        this.animationList = new Animations();
+        this.mouse = { x: 0, y: 0 };
+        this.isDragging = false;
+        this.touchVertex = undefined;
+        this.touchEdge = undefined;
+        this.selectedVertex = undefined;
+        this.autoName = 0;
+        this.mode = Modes.moveVertex;
+        this.textEl = textEl;
+        this.text = "";
+        this.cursor = "default";
+        this.canvas.addEventListener('onModeChange', (e) => this.onModeChange(e));
+        this.canvas.onclick = (e) => this.mouseClick(e);
+        this.canvas.onmousedown = (e) => this.mouseDown(e);
+        this.canvas.onmouseup = (e) => this.mouseUp(e);
+        this.canvas.onmousemove = (e) => this.mouseMove(e);
+        addEventListener('resize', () => this.resize());
+        this.resize();
+    }
+
+    resize() {
+        this.canvas.width = this.proportion * window.innerWidth;
+        this.canvas.height = this.proportion * window.innerHeight;
+        this.reDraw();
+    }
+
+    reDraw() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.graph.draw();
+    }
+
+    setText(str: string) {
+        if (str === this.text) return;
+        this.textEl.textContent = str;
+    }
+
+    setMode(newMode: number) {
+        if (newMode === this.mode) return;
+
+        const oldMode = this.mode;
+        this.mode = newMode;
+        this.canvas.dispatchEvent(
+            new CustomEvent("onModeChange", {detail: {
+                oldMode,
+                newMode
+            }})
+        );
+    }
+
+    setCursor = (c: string) => {
+        if (this.cursor === c) return;
+        this.canvas.style.cursor = c;
+    }
+
+    onModeChange(e: any) {
+        if (this.selectedVertex) {
+            this.selectedVertex.changeColor(this.ctx);
+            this.selectedVertex = undefined;
+        }
+
+        this.reDraw();
+
+        switch (this.mode) {
+            case Modes.moveVertex:
+                this.setText("CLICK AND DRAG THE DESIRED VERTEX");
+                break;
+
+            case Modes.newVertex:
+                this.setText("CLICK TO ADD NEW VERTEX");
+                break;
+
+            case Modes.newEdge:
+                this.setText("SELECT FIRST VERTEX");
+                break;
+
+            case Modes.deleteVertex:
+                this.setText("SELECT VERTEX TO REMOVE");
+                break;
+
+            case Modes.deleteEdge:
+                this.setText("SELECT EDGE TO REMOVE");
+                break;
+
+            case Modes.connectedComponents:
+                this.setText("EACH COLORED SQUARE REPRESENTS A CONNECTED COMPONENT");
+                this.graph.connectedComponents().forEach(component =>
+                    drawPerimeter(
+                        this.ctx,
+                        component.vertices as Circle[],
+                        randomHexadecimalColor()
+                    )
+                );
+                break;
+
+            case Modes.dfs:
+                this.setText("SELECT START VERTEX");
+                break;
+
+            case Modes.bfs:
+                this.setText("SELECT START VERTEX");
+                break;
+
+            case Modes.bipartiteGraph:
+                this.setText(this.graph.bipartiteGraph() ? "TRUE" : "FALSE");
+                break;
+        }
+    }
+
+    drawPath(ctx: CanvasRenderingContext2D, f: any, startVertex: Vertex) {
+        this.reDraw();
+
+        const r = search({
+            graph: this.graph,
+            startVertex,
+            searchAlgorithm: f
+        });
+
+        const path = r as Circle[];
+        path.forEach((vertex: Circle, index: number) => {
+            let animation = vertex.outline;
+
+            if (index === path.length - 1) { //last animation
+                animation = (ctx) => {
+                    this.setText("SELECT START VERTEX");
+                    vertex.outline(ctx);
+                }
+            }
+
+            this.animationList.addAnimation({
+                obj: vertex,
+                animation,
+                params: [ctx],
+                duration: 1000
+            })
+        })
+    }
+
+    mouseMove(e: MouseEvent) {
+        // @ts-ignore 
+        const canvasObj = e.target.getBoundingClientRect();
+        this.mouse.x = e.clientX - canvasObj.left;
+        this.mouse.y = e.clientY - canvasObj.top;
+
+        this.touchVertex = this.graph.vertices.find((circle) => {
+            return distance(this.mouse, circle) <= circle.radius;
+        })
+
+        this.touchEdge = this.graph.edges.find(line => {
+            const linePointNearestMouse = linePointNearestPoint(line, this.mouse);
+            if (!linePointNearestMouse) return false;
+            return distance(this.mouse, linePointNearestMouse) <= line.thickness;
+        });
+
+        if (this.touchVertex || this.touchEdge) {
+            if(this.mode === Modes.moveVertex) {
+                this.setCursor("grab");
+            } else {
+                this.setCursor("pointer");
+            }
+        } else {
+            this.setCursor("auto");
+        }
+
+        if (this.isDragging) {
+            if (!this.selectedVertex) return;
+
+            this.selectedVertex.updatePosition(this.mouse.x, this.mouse.y);
+            this.reDraw();
+        }
+    }
+
+    mouseClick(e: MouseEvent) {
+        // console.log(g)
+
+        switch (this.mode) {
+            case Modes.newVertex:
+                this.graph.addVertex(new Circle(this.autoName.toString(), this.mouse.x, this.mouse.y))
+                this.autoName++;
+                break;
+
+            case Modes.newEdge:
+                if (!this.touchVertex) return;
+
+                if (!this.selectedVertex) {
+                    this.selectedVertex = this.touchVertex;
+                    this.selectedVertex.changeColor(this.ctx, 'red');
+                    this.setText("SELECT SECOND VERTEX");
+                } else {
+                    this.graph.addEdge(new Line(this.selectedVertex, this.touchVertex));
+                    this.selectedVertex.changeColor(this.ctx);
+                    this.selectedVertex = undefined;
+                    this.setText("SELECT FIRST VERTEX");
+                }
+
+                break;
+
+            case Modes.deleteVertex:
+                if (!this.touchVertex) return;
+
+                this.selectedVertex = this.touchVertex;
+                this.graph.deleteVertex(this.selectedVertex);
+                this.selectedVertex = undefined;
+                this.reDraw();
+                break;
+
+            case Modes.deleteEdge:
+                if (!this.touchEdge) return;
+
+                this.graph.deleteEdge(this.touchEdge);
+                this.reDraw();
+                break;
+
+            case Modes.dfs:
+                if (!this.touchVertex) return;
+
+                this.setText("RUNNING DFS");
+                this.drawPath(this.ctx, dfs, this.touchVertex);
+                break;
+
+            case Modes.bfs:
+                if (!this.touchVertex) return;
+
+                this.setText("RUNNING BFS");
+                this.drawPath(this.ctx, bfs, this.touchVertex);
+                break;
+        }
+    }
+
+    mouseDown(e: MouseEvent) {
+        switch (this.mode) {
+            case Modes.moveVertex:
+                if (!this.touchVertex) return;
+
+                this.selectedVertex = this.touchVertex;
+                this.selectedVertex.changeColor(this.ctx, 'red')
+                this.isDragging = true;
+                break;
+        }
+    }
+
+    mouseUp(e: MouseEvent) {
+        if (this.isDragging) {
+            this.isDragging = false;
+
+            if (!this.selectedVertex) return;
+
+            this.selectedVertex.changeColor(this.ctx)
+            this.selectedVertex = undefined;
+        }
+    }
+}
+
+
+interface Animation {
+    obj: any
+    animation: any
+    params: any
+    duration: number
+}
+
+class Animations {
+    list: Animation[]
+    startTime: number
+    currentDuration: number
+
+    constructor() {
+        this.list = [];
+        this.startTime = performance.now();
+        this.currentDuration = 0;
+    }
+
+    addAnimation(animation: Animation) {
+        this.list.push(animation)
+
+        if (this.list.length === 1) {
+            requestAnimationFrame(() => this.animate())
+        }
+    }
+
+    animate() {
+        const now = performance.now();
+        const elapsedTime = now - this.startTime;
+
+        if (elapsedTime >= this.currentDuration) {
+            const item = this.list.shift();
+
+            if (!item) return;
+
+            const { obj, animation, params, duration } = item;
+            obj.animation = animation;
+            obj.animation(...params);
+            this.startTime = performance.now();
+            this.currentDuration = duration;
+        }
+
+        requestAnimationFrame(() => this.animate());
+    }
+}
